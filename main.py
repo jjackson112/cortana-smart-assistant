fbabfe437a485d196b355e133df5cebab50c5ad5
jjackson112 <jjackson11211@gmail.com>
2026-01-05 16:07:10 -0500

    while + for loops added

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # What will Cortana do as my assistant?
 # She should be a program that can remember and respond
 
+# while loop (conditionals) - command loop
+# runs until condition is met - control structure
+# for key, value lists everything in memory
+# for loop for inside commands or methods
+
 class Cortana:
     def __init__(self, name):
         self.name = name
@@ -9,6 +14,16 @@ class Cortana:
     def greeting(self):
         print(f"Hi, my name is {self.name}")
 
+    def list_memory(self):
+        if not self.memory:
+            print("I don't recall this.")
+        else:
+            for key, value in self.memory.items():
+                print(f"{key}: {value}")
+
 if __name__ == "__main__":
     cortana = Cortana("Cortana")
     cortana.greeting()
+
+
+
