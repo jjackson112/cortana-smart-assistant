340c9bc2c400eafbee2f5807b45944bd8b3367a1
jjackson112 <jjackson11211@gmail.com>
2026-01-06 18:30:33 -0500

    contact list started

---
+++ contacts.py
@@ -0,0 +1,11 @@
+class ContactList:
+
+  def __init__(self, name, phone_number, job, birthday):
+    self.name = name
+    self.phone_number = phone_number
+    self.job = job
+    self.birthday = birthday
+
+    
+
+   
