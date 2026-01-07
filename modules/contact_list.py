d115e595e2af03cdbaafa986f890ee9707da3570
jjackson112 <jjackson11211@gmail.com>
2026-01-07 10:17:26 -0500

    add contact function

--- modules/contact_list.py
+++ modules/contact_list.py
@@ -4,7 +4,7 @@ import json
 
 class ContactList:
 
-  def __init__(self, file_path="contacts.path"):
+  def __init__(self, file_path="contacts.json"):
     self.file_path = file_path()
     self.contacts = self.load_contacts()
 
@@ -12,6 +12,28 @@ class ContactList:
     try:
       with open(self.file_path, "r") as file:
         return json.load(file)
-    except:
+    except json.JSONDecodeError:
+      print("File corrupted. Starting fresh.")
       return []
-   
+    
+  def save_contacts():
+    with open(self.file_path, "w") as file:
+      json.dump(self.contacts, file, indent=4)
+
+# add contacts
+def add_contacts(self, name, phone_number, job):
+  self.contacts.append({
+    "name": name,
+    "phone_number": phone_number,
+    "job": job
+  })
+  self.save_contacts()
+
+# search contacts 
+
+# update contacts
+
+
+  
+
+# delete contacts
