fec286d745b560b0e6851b8ba8b332844ed93fb0
jjackson112 <jjackson11211@gmail.com>
2026-01-07 16:22:17 -0500

    add task function

--- modules/to_do_list.py
+++ modules/to_do_list.py
@@ -2,4 +2,19 @@ import json
 
 class ToDo:
     def __init__(self, tasks):
-        self.tasks = tasks
+        self.tasks = []
+
+    def greeting(self):
+        print("What's on your to do list? ")
+
+    def add_task(self, task_name):
+        self.tasks.append({
+            "title": task_name,
+            "completed": False
+        })
+
+    def show_list(self):
+
+    def update_task(self):
+
+    def delete_task(self):
