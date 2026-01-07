a6fa3ac428f71383c7c029e5e7dfbb44c314ee7f
jjackson112 <jjackson11211@gmail.com>
2026-01-06 22:04:46 -0500

    no need for arguments in constructor function

--- modules/contact_list.py
+++ modules/contact_list.py
@@ -1,16 +1,12 @@
 import json
 
+# normalize data after json load
+
 class ContactList:
 
-  def __init__(self, name, phone_number, job):
-    self.name = name
-    self.phone_number = phone_number
-    self.job = job
-
-  with open("contacts.json", "r") as file:
-    contact_list_data = json.load(file)
-
-    print(contact_list_data)
+  def __init__(self, file_path="contacts.path"):
+    self.file_path = file_path()
+    self.contacts = self.load_contacts()
 
 
    
