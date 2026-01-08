a335610fd458f974560c69e76d6fba4ac28feb5b
jjackson112 <jjackson11211@gmail.com>
2026-01-08 11:50:35 -0500

    updated task input

--- modules/to_do_list.py
+++ modules/to_do_list.py
@@ -25,6 +25,19 @@ class ToDo:
         
         select = input("Enter the task number to update:")
 
+        if not select.isdigit():
+            print("Enter a valid number.")
+            return
+        
+        if index < 0 or index >= len(self.tasks):
+            print("Task number out of range.")
+            return
+        
+        updated_task = input("Enter the updated task").strip()
+            if not updated_task:
+            print("Task cannot be empty.")
+            return
+        
     def delete_task(self, index):
         self.tasks.pop(index)
         
