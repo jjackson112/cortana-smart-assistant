5e60ed1023566e861f8e222e2292cc2429fd452c
jjackson112 <jjackson11211@gmail.com>
2026-01-07 22:19:00 -0500

    update add task function

--- modules/to_do_list.py
+++ modules/to_do_list.py
@@ -7,15 +7,18 @@ class ToDo:
     def greeting(self):
         print("What's on your to do list? ")
 
-    def add_task(self, task_name):
-        self.tasks.append({
-            "title": task_name,
-            "completed": False
-        })
+    def add_task(self):
+        task = input("Add a new task: ").strip()
+        if not task:
+            print("It cannot be empty.")
+            return
+        self.tasks.append(task)
+        print("Task added")
 
-    def update_task(self):
+    def update_task(self, index):
 
-    def delete_task(self):
+    def delete_task(self, index):
+        self.tasks.pop(index)
     
     def show_list(self):
         return self.tasks
