9aabf96bb5ecf3038c808decba5dc6605a24336d
jjackson112 <jjackson11211@gmail.com>
2026-01-14 18:05:53 -0500

    first attempt for activity route

--- services/activity.py
+++ services/activity.py
@@ -3,7 +3,9 @@
 # create a Blueprint - GET
 # where will the activity live - memory or persistence?
 
-from flask import Blueprint, requests, jsonify
+from flask import Blueprint, request, jsonify
+from app import db
+from models import Contacts
 
 activity_bp = Blueprint("activity", __name__, url_prefix='/api/activity')
 
@@ -15,4 +17,20 @@ state = {
     "schedule": []
 }
 
-# each activity gets its own endpoint
+# each activity for each mode (CUD -no R) gets its own endpoint
+
+@activity_bp.route("/contacts", methods=["POST"])
+def create_contact():
+    data = request.get_json()
+
+    contact = Contacts(
+        mode = data["mode"],
+        name = data["name"],
+        phone = data["phone"],
+        job = data["job"]
+    )
+
+    db.session.add(contact)
+    db.session.commit()
+
+    return jsonify(contact.to_dict()), 201
