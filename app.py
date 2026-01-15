a1bfc6ed6d3d57cbe65c36400417059f9b1479c2
jjackson112 <jjackson11211@gmail.com>
2026-01-14 23:43:33 -0500

    register activity route + refine app.py

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 # routes are registered here
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from extensions import db
 from routes.status import status_bp
 from routes.health import health_bp
+from services.activity import activity_bp
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cortana.db"
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cortana.db"
-db = SQLAlchemy(app)
+    app.register_blueprint(status_bp)
+    app.register_blueprint(health_bp)
+    app.register_blueprint(activity_bp)
 
-app.register_blueprint(status_bp)
-app.register_blueprint(health_bp)
-
-@app.route("/")
-def home():
-    return "Cortana is running"
-
-
-if __name__ == "__main__":
     with app.app_context():
         db.create_all()
+    
+    return app
 
+if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
