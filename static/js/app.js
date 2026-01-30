40a3f3429e8cf722862825e7f1283353b51fb866
jjackson112 <jjackson11211@gmail.com>
2026-01-30 17:22:24 -0500

    minor issues

---
+++ static/js/app.js
@@ -0,0 +1,6 @@
+const executeBtn = getElementById("execute");
+const showInput = document.querySelector("input");
+
+button.addEventListener = ("click", function(e) {
+    showInput.classList.show
+})
