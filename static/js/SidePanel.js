2ef88bb1fa6f2e595ad213707a6af6189171f3c9
jjackson112 <jjackson11211@gmail.com>
2026-02-01 15:17:59 -0500

    sidepanel.jscomponent

--- static/js/SidePanel.js
+++ static/js/SidePanel.js
@@ -1,2 +1,20 @@
 import ActivityList from "./ActivityList";
 
+export default function SidePanel({ activities }) {
+    return (
+        <aside className="side-panel">
+            {/* Cortana Response Area */}
+            <div className="response-area">
+                <h2 className="text-2xl font-semibold mb-2">Response</h2>
+                <p className="text-cyan-400 font-semibold mb-4">Cortana is ready</p>
+                <p id="cortana-reply"></p>
+            </div>
+
+            {/* Activity Log */}
+            <div className="activity-log-area">
+                <h2 className="text-xl font-semibold mb-2">Activity Log</h2>
+                <ActivityList activities={activities}/> 
+            </div>
+        </aside>
+    )
+}
