6323a44c8663d89a4f976cde55d236fe5e398ebb
jjackson112 <jjackson11211@gmail.com>
2026-02-13 17:23:12 -0500

    jsonify to replace util helper methods

--- backend/routes/assistant.py
+++ backend/routes/assistant.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, request
-from utils.response import success, error_response
+from flask import Blueprint, request, jsonify
 from assistant_core import to_do_list_mode
 
 assistant_bp = Blueprint("assistant", __name__, url_prefix="/api")
@@ -10,11 +9,11 @@ def assistant_command():
         return {"status": "assistant route is alive"}
     data = request.get_json()
     if not data:
-        return error_response("Invalid JSON", 400)
+        return jsonify("Invalid JSON", 400)
     
     input_text = data.get("command")
     state = data.get("state")
 
     print("TESTING")
     result = to_do_list_mode(input_text=input_text, state=state)
-    return success(result), 200
+    return jsonify(result), 200
