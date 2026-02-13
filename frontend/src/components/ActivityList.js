520f9dc9a32745ce578ecedbaf3774ea67277899
jjackson112 <jjackson11211@gmail.com>
2026-02-13 18:00:38 -0500

    move displayActivities to ActivityList.js

--- frontend/src/components/ActivityList.js
+++ frontend/src/components/ActivityList.js
@@ -2,14 +2,17 @@ import ActivityItem from "./ActivityItem"
 /*array data model */
 
 export default function ActivityList({ activities }) {
+  // Show only most recent 5 activity logs 
+  const displayActivities = [...activities].slice(-5).reverse();
+
   if (!activities.length) {
     return <p>No activity yet.</p>
   }
 
   return (
     <ul className="activity-list">
-      {activities.map(a => (
-        <ActivityItem key={a.id} activity={a} />
+      {displayActivities.map((activity) => (
+        <ActivityItem key={activity.id} activity={activity} />
       ))}
     </ul>
   )
