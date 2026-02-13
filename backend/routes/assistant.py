8559670fe684db2649626d065bb92e2e442756c3
jjackson112 <jjackson11211@gmail.com>
2026-02-13 17:39:43 -0500

    remove testing from assistant route

--- backend/routes/assistant.py
+++ backend/routes/assistant.py
@@ -8,8 +8,6 @@ assistant_bp = Blueprint("assistant", __name__, url_prefix="/api")
 
 @assistant_bp.route("/assistant", methods=["POST"])
 def assistant_command():
-    if request.method == "GET":
-        return {"status": "assistant route is alive"}
     data = request.get_json()
     if not data:
         return jsonify("Invalid JSON", 400)
@@ -17,6 +15,5 @@ def assistant_command():
     input_text = data.get("command")
     state = data.get("state")
 
-    print("TESTING")
     result = to_do_list_mode(input_text=input_text, state=state)
     return jsonify(result), 200
