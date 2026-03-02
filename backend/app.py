2c41b1e7caea50808c8731423df9b96692769134
jjackson112 <jjackson11211@gmail.com>
2026-03-02 10:24:15 -0500

    added test to print in app.py

--- backend/app.py
+++ backend/app.py
@@ -30,7 +30,6 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-
     # bind extensions
     db.init_app(app)
 
@@ -46,6 +45,9 @@ def create_app():
 
     with app.app_context():
         db.create_all()
+
+    for rule in app.url_map.iter_rules():
+        print(rule)
 
     return app
 
